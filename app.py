7b1e324ade615522b8e7fc657a3c7586664cb726
esapkus <168883394+esapkus@users.noreply.github.com>
2026-04-20 23:47:38 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-
-class Handler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Hello Sandip - DevOps Project")
-
-HTTPServer(("", 5000), Handler).serve_forever()
